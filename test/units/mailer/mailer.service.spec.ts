0bec5b777e7525258ce8c55e46823cf20ca3591c
Antonio Aviles <71448936+Antonio-112@users.noreply.github.com>
2023-05-19 20:01:32 -0400

    test: added test

--- test/units/mailer/mailer.service.spec.ts
+++ test/units/mailer/mailer.service.spec.ts
@@ -1,10 +1,12 @@
-import { HttpStatus } from '@nestjs/common';
-import { MailerService } from '@nestjs-modules/mailer';
-import { NestMailerService } from 'src/mailer/mailer.service';
 import { Test } from '@nestjs/testing';
+import { MailerService } from '@nestjs-modules/mailer';
+import { NestMailerService } from '../../../src/mailer/mailer.service';
+import { ISendMailOptions } from '@nestjs-modules/mailer/dist/interfaces/send-mail-options.interface';
+import { HttpStatus, HttpException } from '@nestjs/common';
+
 describe('NestMailerService', () => {
+  let service: NestMailerService;
   let mailerService: MailerService;
-  let nestMailerService: NestMailerService;
 
   beforeEach(async () => {
     const moduleRef = await Test.createTestingModule({
@@ -13,46 +15,65 @@ describe('NestMailerService', () => {
         {
           provide: MailerService,
           useValue: {
-            sendMail: jest.fn().mockResolvedValue(undefined),
+            sendMail: jest.fn().mockResolvedValue(true),
           },
         },
       ],
     }).compile();
 
-    nestMailerService = moduleRef.get<NestMailerService>(NestMailerService);
+    service = moduleRef.get<NestMailerService>(NestMailerService);
     mailerService = moduleRef.get<MailerService>(MailerService);
   });
 
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
   describe('sendEmail', () => {
-    it('debería enviar el correo exitosamente', async () => {
-      const mailOptions = {
-        to: 'destinatario@example.com',
-        from: 'remite@example.com',
-        subject: 'Correo de prueba',
-        text: 'Este es un correo de prueba.',
+    it('should send email and return success message', async () => {
+      const sendMailOptions: ISendMailOptions = {
+        to: 'test@example.com',
+        from: 'noreply@example.com',
+        subject: 'Test Email',
+        text: 'This is a test email',
       };
 
-      jest.spyOn(mailerService, 'sendMail').mockResolvedValueOnce(undefined);
+      const response = await service.sendEmail(sendMailOptions);
 
-      const result = await nestMailerService.sendEmail(mailOptions);
-
-      expect(result.statusCode).toBe(HttpStatus.OK);
-      expect(result.message).toBe('¡Correo enviado exitosamente!');
+      expect(mailerService.sendMail).toBeCalledWith(sendMailOptions);
+      expect(response).toEqual({
+        statusCode: HttpStatus.OK,
+        message: 'Email successfully sent!',
+      });
     });
 
-    it('debería manejar un error de sobre de correo electrónico inválido', async () => {
-      const mailOptions = {
-        to: 'destinatario@example.com',
-        from: 'dirección_de_correo_inválida',
-        subject: 'Correo de prueba',
-        text: 'Este es un correo de prueba.',
+    it('should throw error if email address is invalid', async () => {
+      const sendMailOptions: ISendMailOptions = {
+        to: 'invalidEmail',
+        from: 'noreply@example.com',
+        subject: 'Test Email',
+        text: 'This is a test email',
       };
 
-      const result = await nestMailerService.sendEmail(mailOptions);
+      await expect(service.sendEmail(sendMailOptions)).rejects.toThrow(
+        HttpException,
+      );
+    });
 
-      expect(result.statusCode).toBe(HttpStatus.BAD_REQUEST);
-      expect(result.message).toBe(
-        'Sobre del correo electrónico inválido. Por favor, verifica las direcciones de correo electrónico.',
+    it('should handle failure of email sending operation', async () => {
+      const sendMailOptions: ISendMailOptions = {
+        to: 'test@example.com',
+        from: 'noreply@example.com',
+        subject: 'Test Email',
+        text: 'This is a test email',
+      };
+
+      jest
+        .spyOn(mailerService, 'sendMail')
+        .mockRejectedValue(new Error('Failed to send email'));
+
+      await expect(service.sendEmail(sendMailOptions)).rejects.toThrow(
+        HttpException,
       );
     });
   });
